695e07b090556112d02fcc0eab8ee547ff67d60d
cintyanadaa <cintyanadaa17@gmail.com>
2020-11-10 19:50:42 +0700

    publish file index

---
+++ AccessModifier/src/com/dicoding/javafundamental/accessmodifier/KelasA.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.accessmodifier;
+
+/**
+ *
+ * @author Sindy
+ */
+public class KelasA {
+    private int memberA = 5;
+    
+    char memberB = 'A';
+    double memberC = 1.5;
+    
+    private int functionA() {
+        return memberA;
+    }
+ 
+    int functionB() {
+        // Pemanggilan private member dan private function
+        int hasil = functionA() + memberA;
+        return hasil;
+    }
+    
+}
